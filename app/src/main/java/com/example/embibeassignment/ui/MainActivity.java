fe7315f8789ec7205613a6f71e4f84c7e9b75679
dilipchandar <dilipchandar89@gmail.com>
2020-09-11 12:05:23 +0800

    Code cleanup

--- app/src/main/java/com/example/embibeassignment/ui/MainActivity.java
+++ app/src/main/java/com/example/embibeassignment/ui/MainActivity.java
@@ -73,8 +73,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setTitle("Search by title");
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Search by title");
+        }
 
         recyclerView = findViewById(R.id.recycler_view);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
